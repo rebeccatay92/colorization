b8743efbaea0aae603520c7dacfdae0098544ec9
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-23 15:05:53 +0800

    html has a score and a button. score increases whenever button is clicked.

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 function randNum () {
   return Math.floor(Math.random() * 256)
 }
-
+var counter = 0
+var score = document.querySelector('.score')
 document.querySelector('div').addEventListener('click', colorize)
+document.querySelector('div').addEventListener('click', function() {
+  counter ++
+  score.innerText = counter
+})
 
 function colorize () {
   document.querySelector('body').style.backgroundColor = 'rgba(' + randNum() + ',' + randNum() + ',' + randNum() + ',' + randNum() + ')'
