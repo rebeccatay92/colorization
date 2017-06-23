9d7bdcefa6fb838f30dfa24c9473624d0034baaa
Nicholas Lee <niconal902@gmail.com>
2017-06-23 15:24:08 +0800

    colors change based on colors in array

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+var colors = ['yellow', 'red', 'green', 'blue', 'purple']
+var color = ''
 // returns number between 0 and 255
 function randNum () {
-  return Math.floor(Math.random() * 256)
+  return Math.floor(Math.random() * 6)
 }
+
 var counter = 0
 var score = document.querySelector('.score')
+
+// click to change color
 document.querySelector('div').addEventListener('click', colorize)
-document.querySelector('div').addEventListener('click', function() {
-  counter ++
+
+document.querySelector('div').addEventListener('click', function () {
+  // if (document.querySelector('div').textcontent === color) {
+  counter++
   score.innerText = counter
+  // }
 })
 
 function colorize () {
-  document.querySelector('body').style.backgroundColor = 'rgba(' + randNum() + ',' + randNum() + ',' + randNum() + ',' + randNum() + ')'
+  var randStore = randNum()
+  document.querySelector('body').style.backgroundColor = colors[randStore]
+  color = colors[randStore]
 }
